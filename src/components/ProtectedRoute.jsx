4aedb6e2f8c885d453c8d1ee48baf524aab02077
spshreyash <spshreyash2002@gmail.com>
2026-04-24 15:09:17 +0530

    protected Route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../Store/AuthContext';
+import { Loading } from '../pages/Loading';
 
 
 export const ProtectedRoute = ({ children, allowedRoles }) => {
- const{role,user}=useContext(AuthContext)
+ const{role,user, loading}=useContext(AuthContext)
   console.log("ProtectedRoute - user:", user);
   console.log("ProtectedRoute - role:", role);
+   console.log("ProtectedRoute - loading:", loading);
 
+  if (loading) {
+  return <> <Loading/> </>;
+}
   if (!user) {
     return <Navigate to="/" />;
   }
