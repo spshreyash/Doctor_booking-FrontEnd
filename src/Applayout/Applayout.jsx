333f7595d71717b36395a952fecc495cb4378783
spshreyash <spshreyash2002@gmail.com>
2026-04-24 12:04:56 +0530

    applayout

--- src/Applayout/Applayout.jsx
+++ src/Applayout/Applayout.jsx
@@ -10,12 +10,12 @@ import {Loading} from "../pages/Loading"
 
 export const Applayout = () => {
 
-  const navigation=useNavigation()
-  console.log(" this si sloading navigation",navigation)
-  if(navigation.state === "loading")
-  {
-    return <Loading/>
-  }
+  // const navigation=useNavigation()
+  // // console.log(" this si sloading navigation",navigation)
+  // if(navigation.state === "loading")
+  // {
+  //   return <Loading/>
+  // }
   return (
     <>
       <Heder/>
